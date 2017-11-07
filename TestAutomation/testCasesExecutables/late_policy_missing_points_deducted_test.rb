a297cfb6067b6fc43f1d855ef737457bdff2373c
Sam Word <samword245@gmail.com>
2017-11-07 15:16:49 -0500

    Added skeleton executable files

---
+++ TestAutomation/testCasesExecutables/late_policy_missing_points_deducted_test.rb
@@ -0,0 +1,27 @@
+#=======================================================#
+# Team Name:    Don't Test Me                           #
+# Team Members: Kenneth Dela Cruz, Kyle Glick, Sam Word #
+#=======================================================#
+
+# Require Testing Class
+require_relative '../project/testatron'
+
+# Require Necessary Canvas Classes
+require_relative '../project/late_policy'
+
+# Require Necessary Libraries
+require 'json'
+
+class LatePolicyMissingPointsDeductedTest < Testatron
+  
+	def initialize
+		super(2)
+	end
+
+	def run
+
+		super()
+	end
+end
+
+LatePolicyMissingPointsDeductedTest.new().run()
