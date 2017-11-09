36b79ac3d960e8e2061f3f81cc6e00cff370a162
Kyle Glick <kyleglick99@gmail.com>
2017-11-09 15:32:28 -0500

    add tests / fix test case oracles for late policy missing points deducted

--- TestAutomation/testCasesExecutables/late_policy_missing_points_deducted_test.rb
+++ TestAutomation/testCasesExecutables/late_policy_missing_points_deducted_test.rb
@@ -15,12 +15,13 @@ require 'json'
 class LatePolicyMissingPointsDeductedTest < Testatron
   
 	def initialize
-		super(2)
+		super(3)
 	end
 
 	def run
 		late_policy = LatePolicy.new()
-		super(true)
+                late_policy.missing_submission_deduction = @params[0]
+		super(late_policy.missing_points_deducted(@params[1], @params[2]).to_s)
 	end
 end
 
